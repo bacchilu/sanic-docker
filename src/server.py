a1fceb5375ddb6761c76b158038d544bbb8c92e4
Luca Bacchi <bacchilu@gmail.com>
2023-03-15 16:05:38 +0100

    JSON response

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 from sanic import Sanic
 from sanic.request import Request
-from sanic.response import text
+from sanic.response import json, text
 
 app = Sanic("MyHelloWorldApp")
 
@@ -8,3 +8,8 @@ app = Sanic("MyHelloWorldApp")
 @app.get("/")
 async def hello_world(request: Request):
     return text(f"Hello, Sanic! - 1.0.0 ({app.config.MODE})")
+
+
+@app.route("/json")
+async def json_handler(request):
+    return json({"hello": "world"})
